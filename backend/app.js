e25321c8caa8575782a74354c64bf39336e4297f
Amr995 <amr995@yahoo.com>
2024-12-21 21:25:22 +0200

    add login user part

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ app.use("/api/auth", require("./routers/authRoute"));
 // Running The Server
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () =>
-    console.log(
-      `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
-    )
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+  )
 );
--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,39 +1,84 @@
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcryptjs");
-const { User, validateRegisterUser } = require("../models/User");
+const {
+  User,
+  validateRegisterUser,
+  validateLoginUser,
+} = require("../models/User");
 
 /**______________________________________________________
  * @desc Register New User - Sing Up
- * @router /api/auth/register
+ * @route /api/auth/register
  * @method POST
  * @access public
  ______________________________________________________*/
 
- module.exports.registerUserCtrl = asyncHandler(async (req, res) => {
-    const { error } = validateRegisterUser(req.body);
-    if(error) {
-        return res.status(400).json({ message: error.details[0].message });
-    }
+module.exports.registerUserCtrl = asyncHandler(async (req, res) => {
+  const { error } = validateRegisterUser(req.body);
+  if (error) {
+    return res.status(400).json({ message: error.details[0].message });
+  }
 
-    let user = await User.findOne({email: req.body.email});
-    if(user) {
-        return res.status(400).json({message: "user already exist"});
-    }
+  let user = await User.findOne({ email: req.body.email });
+  if (user) {
+    return res.status(400).json({ message: "user already exist" });
+  }
 
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(req.bady.password, salt);
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.bady.password, salt);
 
-    user = new User({
-        username: req.body.username,
-        email: req.body.email,
-        password: hashedPassword,
-    });
+  user = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: hashedPassword,
+  });
 
-    await user.save();
+  await user.save();
 
-    res.status(201).json({message: "You're registered successfully, Please log in"})
-    // is user already exists
-    // hash the password
-    // new user and save it to Db
-    // send a response to client
+  //@TODO - sending email (verify account)
+
+  res
+    .status(201)
+    .json({ message: "You're registered successfully, Please log in" });
+  // is user already exists
+  // hash the password
+  // new user and save it to Db
+  // send a response to client
+});
+
+/**______________________________________________________
+ * @desc Login User
+ * @route /api/auth/login
+ * @method POST
+ * @access public
+ ______________________________________________________*/
+
+module.exports.loginUserCtrl = asyncHandler(async (req, res) => {
+  const { error } = validateLoginUser(req.body);
+  if (error) {
+    return res.status(400).json({ message: error.details[0].message });
+  }
+
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    return res.status(400).json({ message: "invalid email or password" });
+  }
+
+  const isPasswordMatch = await bcrypt.compare(
+    req.body.password,
+    user.password
+  );
+  if (!isPasswordMatch) {
+    return res.status(400).json({ message: "invalid email or password" });
+  }
+
+  //@TODO - sending email (verify account if not verified)
+  const token = user.generateAuthToken();
+
+  res.status(200).json({
+    _id: user._id,
+    isAdmin: user.isAdmin,
+    profilePhoto: user.profilePhoto,
+    token,
+  });
 });
