99e75e8eac07f94589773bf9b0100f21412d3202
Amr995 <amr995@yahoo.com>
2024-12-31 20:08:33 +0200

    React Routering

--- frontend/src/components/header/Navbar.jsx
+++ frontend/src/components/header/Navbar.jsx
@@ -1,19 +1,21 @@
+import { Link } from "react-router-dom";
+
 const Navbar = ({ toggle, setToggle }) => {
     return ( 
         <nav style={{clipPath: toggle && "polygon(0 0, 100% 0, 100% 100%, 0 100%)" }} className="navbar">
             <ul className="nav-links">
-                <li onClick={() => setToggle(false)} className="nav-link">
+                <Link to="/" onClick={() => setToggle(false)} className="nav-link">
                     <i className="bi bi-house"></i> Home
-                </li>
-                <li onClick={() => setToggle(false)} className="nav-link">
+                </Link>
+                <Link to="/posts" onClick={() => setToggle(false)} className="nav-link">
                     <i className="bi bi-stickies"></i> Posts
-                </li>
-                <li onClick={() => setToggle(false)} className="nav-link">
+                </Link>
+                <Link to="/posts/create-post" onClick={() => setToggle(false)} className="nav-link">
                     <i className="bi bi-journal-plus"></i> Create
-                </li>
-                <li onClick={() => setToggle(false)} className="nav-link">
+                </Link>
+                <Link to="/admin-dashboard" onClick={() => setToggle(false)} className="nav-link">
                     <i className="bi bi-person-check"></i> Admin Dashboard
-                </li>
+                </Link>
                 </ul>
         </nav>
      );
