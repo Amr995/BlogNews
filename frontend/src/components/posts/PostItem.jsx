18c493e49ebec84996967a33073792bbe89ea0ea
Amr995 <amr995@yahoo.com>
2025-01-01 00:13:07 +0200

    Home page and Posts

---
+++ frontend/src/components/posts/PostItem.jsx
@@ -0,0 +1,44 @@
+import { Link } from "react-router-dom";
+
+const PostItem = ({post}) => {
+    return ( 
+        <div className="post-item">
+            <div className="post-item-image-wrapper">
+                <img src={post.image} alt="" className="post-item-image" />
+            </div>
+            <div className="post-item-info-wrapper">
+                <div className="post-item-info">
+                    <div className="post-item-author">
+                        <strong>Author: </strong>
+                        <Link className="post-item-username" to="/profile/1">
+                        {post.user.username}
+                        </Link>
+                    </div>
+                    <div className="post-item-date">
+                        {new Date(post.createdAt).toDateString()}
+                    </div>
+                </div>
+                <div className="post-item-details">
+                    <h4 className="post-item-title">{post.title}</h4>
+                    <Link classname="post-item-category" to={`/posts/categories/${post.category}`}>
+                    {post.category}
+                    </Link>
+                </div>
+                <p className="post-item-description">
+                    {post.description}
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Sapiente animi neque aliquid nam totam deserunt ab blanditiis hic explicabo dicta harum cumque,
+                    quis enim aut a cum alias incidunt nisi?
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Sapiente animi neque aliquid nam totam deserunt ab blanditiis hic explicabo dicta harum cumque,
+                    quis enim aut a cum alias incidunt nisi?
+                </p>
+                <Link className="post-item-link" to={`/posts/details${post._id}`}>
+                Read More...
+                </Link>
+            </div>
+        </div>
+    );
+}
+ 
+export default PostItem;
