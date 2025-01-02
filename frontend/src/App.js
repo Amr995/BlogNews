bd899d7ad5fc2482e87b3e17e64ee2c3f796321a
Amr995 <amr995@yahoo.com>
2025-01-02 14:45:43 +0200

    create post form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,21 +4,23 @@ import Home from "./pages/home/Home";
 import Login from "./pages/forms/Login";
 import Register from "./pages/forms/Register";
 import PostsPage from "./pages/posts-page/PostsPage";
-import CreatePosts from "./pages/create-post/CreatePost";
+import CreatePost from "./pages/create-post/CreatePost";
 import AdminDashboard from "./pages/admin/AdminDashboard";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/posts" element={<PostsPage />} />
-          <Route path="/posts/create-post" element={<CreatePosts />} />
-          <Route path="/admin-dashboard" element={<AdminDashboard />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/posts" element={<PostsPage />} />
+        <Route path="/posts/create-post" element={<CreatePost />} />
+        <Route path="/admin-dashboard" element={<AdminDashboard />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
