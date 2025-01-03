0887b96b6953e16020ff8f353fbe0d56c56596f0
Amr995 <amr995@yahoo.com>
2025-01-03 23:46:53 +0200

    Category Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,19 +9,23 @@ import AdminDashboard from "./pages/admin/AdminDashboard";
 import Footer from "./components/footer/Footer";
 import PostDetails from "./pages/post-details/PostDetails";
 import { ToastContainer } from "react-toastify";
+import Category from "./pages/category/Category";
 
 function App() {
   return (
     <BrowserRouter>
-    <ToastContainer theme="colored" position="top-center" />
+      <ToastContainer theme="colored" position="top-center" />
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/posts" element={<PostsPage />} />
-        <Route path="/posts/create-post" element={<CreatePost />} />
-        <Route path="/posts/details/:id" element={<PostDetails/>} />
+        <Route path="posts">
+          <Route index element={<PostsPage />} />
+          <Route path="create-post" element={<CreatePost />} />
+          <Route path="details/:id" element={<PostDetails />} />
+          <Route path="categories/:category" element={<Category />} />
+        </Route>
         <Route path="/admin-dashboard" element={<AdminDashboard />} />
       </Routes>
       <Footer />
